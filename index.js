5f99e0806309e71551905f69b3ac2dba892d9651
Tyler-Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-11-14 00:00:20 -0700

    Now have hashes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let imgElement = document.getElementById("imageSrc")
+let imgElement = document.createElement("img");
+let imgDisp = document.getElementById("imageSrcDisp");
 let inputElement = document.getElementById("fileInput");
 
 inputElement.addEventListener("change", (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
+    imgDisp.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
 imgElement.onload = function() {
@@ -99,6 +101,8 @@ imgElement.onload = function() {
     let vertices = cv.RotatedRect.points(boundingRect);
     let rectangleColor = new cv.Scalar(255, 0, 0, 255);
     let minRectDst = src.clone();
+    console.log("vertices:");
+    console.log(vertices);
     // draw rotatedRect
     for (let i = 0; i < 4; i++) {
         cv.line(minRectDst, vertices[i], vertices[(i + 1) % 4], rectangleColor, 2, cv.LINE_AA, 0);
@@ -106,32 +110,135 @@ imgElement.onload = function() {
 
     cv.imshow('canvasOutput7', minRectDst);
 
-    for (const key in contours.get(cardContourInd)) {
-        console.log(key);
-    }
+    // for (const key in contours.get(cardContourInd)) {
+    //     console.log(key);
+    // }
     console.log(contours.get(cardContourInd).data32S);
-    let circleColor = new cv.Scalar(255, 0, 0, 255);
-    let pointsDst = src.clone();
+    let circleColor1 = new cv.Scalar(255, 0, 0, 255);
+    let pointsDst = minRectDst.clone();
+    let coords = [];
     for (let i = 0; i < contours.get(cardContourInd).data32S.length / 2; i++) {
         const x = contours.get(cardContourInd).data32S[2 * i];
         const y = contours.get(cardContourInd).data32S[2 * i + 1];
         let center = new cv.Point(x, y);
-        console.log("x: " + x + ", y: " + y);
-        cv.circle(pointsDst, center, 3, circleColor);
+        // console.log("x: " + x + ", y: " + y);
+        cv.circle(pointsDst, center, 3, circleColor1);
+        coords.push([x, y]);
+    }
+    // console.log(coords);
+    closestPoints = [];
+    for(let i = 0; i < 4; i++) {
+        const vertex = vertices[i];
+        let tempClosest;
+        let tempClosestDist = 1000;
+        for (const j in coords) {
+            // console.log(coords[j]);
+            let coord = coords[j];
+            let tempDist = Math.sqrt(Math.pow((vertex.x - coord[0]),2) + Math.pow((vertex.y - coord[1]),2));    //calculate current distance
+            if (tempDist < tempClosestDist) {   //this point is closer
+                tempClosestDist = tempDist
+                tempClosest = coord;
+                // console.log(coord);
+            }
+        }
+        // let point = new cv.Point(tempClosest[0], tempClosest[1])
+        // closestPoints.push(point);
+        closestPoints.push([tempClosest[0], tempClosest[1]]);
+    }
+    console.log(closestPoints);
+    let circleColor2 = new cv.Scalar(0, 255, 0, 255);
+    for (let i = 0; i < 4; i++) {
+        let center = new cv.Point(closestPoints[i][0], closestPoints[i][1]);
+        // cv.circle(pointsDst, closestPoints[i], 3, circleColor2);
+        cv.circle(pointsDst, center, 3, circleColor2);
     }
     cv.imshow("canvasOutput8", pointsDst);
 
-    // console.log(vertices);
-    // let height = boundingRect.size.height;
-    // let width = boundingRect.size.width;
-    // let warpDst = [];
-    // warpDst.push = [0, height-1];
-    // warpDst.push = [0, 0];
-    // warpDst.push = [width-1, 0];
-    // warpDst.push = [width-1, height-1];
-    // cv.getPerspectiveTransform()
+    console.log(closestPoints);
 
 
+    let tempVertices = [];
+    // for (let i = 0; i < 4; i++) {
+    //     tempVertices.push(vertices[i].x);
+    //     tempVertices.push(vertices[i].y);
+    // }
+    tempVertices = [0, 0, 630, 0, 630, 880, 0, 880];
+
+
+    // console.log(tempVertices);
+    // let verticesMat = cv.matFromArray(2, 4, cv.CV_32SC1, tempVertices);
+
+    // console.log(closestPointsMat.data32S);
+    // console.log(verticesMat.data32S);
+
+    let width = Math.min(boundingRect.size.height, boundingRect.size.width);
+    let height = Math.max(boundingRect.size.height, boundingRect.size.width);
+    // console.log(width);
+    // console.log(boundingRect.size.height);
+    if (width == boundingRect.size.width) {    //card sideways
+        console.log(tempVertices);
+        tempVertices.unshift(tempVertices.pop());
+        console.log(tempVertices);
+        tempVertices.unshift(tempVertices.pop());
+        console.log(tempVertices);
+    }
+
+    let closestPointsMat = cv.matFromArray(4, 1, cv.CV_32FC2, [].concat(...closestPoints));
+    let verticesMat = cv.matFromArray(4, 1, cv.CV_32FC2, tempVertices);
+    console.log(closestPointsMat);
+    console.log(verticesMat);
+
+    let M = cv.getPerspectiveTransform(closestPointsMat, verticesMat);
+    console.log(M);
+    let dsize = new cv.Size(1000, 1000);
+    let warpDst = new cv.Mat();
+    cv.warpPerspective(src, warpDst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+    cv.imshow("canvasOutput9", warpDst);
+
+    let cropped = new cv.Mat();
+    let rect = new cv.Rect(0, 0, 630, 880);
+    cropped = warpDst.roi(rect);
+    cv.cvtColor(cropped, cropped, cv.COLOR_RGBA2GRAY);
+    cv.imshow("canvasOutput10", cropped);
+    // console.log(cropped.rows);
+    // console.log(cropped.cols);
+    // console.log(cropped.data);
+
+    let hashSize = new cv.Size(8, 8);
+    cv.resize(cropped, cropped, hashSize, 0, 0, cv.INTER_AREA);
+    cv.imshow("canvasOutput11", cropped);
+    console.log(cropped.data);
+    let average = 0;
+    for (let i = 0; i < cropped.data.length; i++) {
+        average += cropped.data[i];
+    }
+    average = average/cropped.data.length;
+    console.log(average);
+    let hashed = new Uint32Array(2);
+    console.log(hashed);
+    let hashedStr = "";
+    for (let i = 0; i < 32; i++) {
+        hashed[0] = hashed[0] << 1;
+        if (cropped.data[i] > average) {
+            hashed[0] = hashed[0] | 1;
+        }
+        hashedStr += hashed[0] & 1;
+    }
+    for (let i = 32; i < 64; i++) {
+        hashed[1] = hashed[1] << 1;
+        if (cropped.data[i] > average) {
+            hashed[1] = hashed[1] | 1;
+        }
+        hashedStr += hashed[1] & 1;
+    }
+    console.log(hashedStr);
+
+
+    cropped.delete();
+    warpDst.delete();
+    M.delete();
+    closestPointsMat.delete();
+    verticesMat.delete();
     pointsDst.delete();
     minRectDst.delete();
     claheDst.delete();
