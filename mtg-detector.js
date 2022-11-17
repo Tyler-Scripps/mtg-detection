07e2a8e212442718423bf7cf5e770f572f2b2187
Tyler Scripps <43222263+Tyler-Scripps@users.noreply.github.com>
2022-11-17 15:32:39 -0700

    now able to output steps for debugging

--- mtg-detector.js
+++ mtg-detector.js
@@ -7,7 +7,7 @@
  * @param {String} [outDisplay] - Optional id of div or canvas that will show the found card
  * @returns Array of uint8s containing grayscale image
  */
-export function processImage(imgElement, outX = 630, outY = 880, outDisplay = "") {
+export function processImage(imgElement, outX = 630, outY = 880, stepElements = {}) {
     let src = cv.imread(imgElement);    //convert image to matrix for manipulation
     let scalef = 1000 / Math.max(src.size().width, src.size().height);  //determine the scale factor to keep the largest dimesnion under 1000px
     if (scalef < 1) {   //if scaling is needed do the scaling, only scales down not up because this is purely to maintain performance
@@ -29,14 +29,32 @@ export function processImage(imgElement, outX = 630, outY = 880, outDisplay = ""
     let srcGray = new cv.Mat(); //will store grayscale normalized image
     cv.cvtColor(normalizedImg, srcGray, cv.COLOR_RGBA2GRAY, 0);   //convert to grayscale
 
+    if (stepElements.gray) {    //output grayscale to element
+        cv.imshow(stepElements.gray, srcGray);
+    }
+
     //thresholding
     let threshDst = new cv.Mat();   //will store the thresholded image
     cv.threshold(srcGray, threshDst, 110, 255, cv.THRESH_BINARY);   //run a threshold over image
+
+    if (stepElements.threshold) {   //output threshold to element
+        cv.imshow(stepElements.threshold, threshDst);
+    }
     
     //contour fnding
     let contours = new cv.MatVector();  //will store found contours
     let hierarchy = new cv.Mat();   //will store contour hierarchy
     cv.findContours(threshDst, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE); //actually finds the contours
+
+    if (stepElements.contours) {    //output contours to element
+        let contourDst = cv.Mat.zeros(threshDst.rows, threshDst.cols, cv.CV_8UC1);
+        for (let i = 0; i < contours.size(); ++i) {
+            let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255));
+            cv.drawContours(contourDst, contours, i, color, 1, cv.LINE_8);
+        }
+        cv.imshow(stepElements.contours, contourDst);
+        contourDst.delete();
+    }
 
     //for more on contour hierarchy: https://docs.opencv.org/4.6.0/da/d0a/tutorial_js_contours_hierarchy.html
     let topLevel = [];  //will store all top level contours
@@ -54,6 +72,14 @@ export function processImage(imgElement, outX = 630, outY = 880, outDisplay = ""
             cardContourInd = i;
             break;
         }
+    }
+
+    if (stepElements.cardContour) { //output card contour to element
+        let contourDst = cv.Mat.zeros(threshDst.rows, threshDst.cols, cv.CV_8UC1);
+        let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255));
+        cv.drawContours(contourDst, contours, cardContourInd, color, 1, cv.LINE_8);
+        cv.imshow(stepElements.cardContour, contourDst);
+        contourDst.delete();
     }
 
     //bounding box
@@ -86,6 +112,21 @@ export function processImage(imgElement, outX = 630, outY = 880, outDisplay = ""
         closestPoints.push([tempClosest[0], tempClosest[1]]);   //store the closest point for this vertex
     }
 
+    if (stepElements.boundingRect) {    //output bounding box and closest point to element
+        let minRectDst = src.clone();
+        let rectangleColor = new cv.Scalar(255, 0, 0, 255);
+        let circleColor = new cv.Scalar(0, 255, 0, 255);
+        for (let i = 0; i < 4; i++) {
+            cv.line(minRectDst, vertices[i], vertices[(i + 1) % 4], rectangleColor, 2, cv.LINE_AA, 0);
+            for (let i = 0; i < 4; i++) {
+                let center = new cv.Point(closestPoints[i][0], closestPoints[i][1]);
+                cv.circle(minRectDst, center, 3, circleColor);
+            }
+        }
+        cv.imshow(stepElements.boundingRect, minRectDst);
+        minRectDst.delete();
+    }
+
     //transformation
 
     let tempVertices = [0, 0, 630, 0, 630, 880, 0, 880];  //predictably located and sized vertices in aspect ratio of card
@@ -103,14 +144,17 @@ export function processImage(imgElement, outX = 630, outY = 880, outDisplay = ""
     let warpDst = new cv.Mat(); //matrix to store transformed image
     cv.warpPerspective(srcGray, warpDst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());   //actually to the warping (deskewing and rotation)
 
-    if (outDisplay.length > 0) {
-        cv.imshow(outDisplay, warpDst)
+    if (stepElements.warp) {    //output (de)warped image to element
+        cv.imshow(stepElements.warp, warpDst)
     }
 
     //the user wants a resize
     if (outX != 630 || outY != 880) {
         let finalSize = new cv.Size(outX, outY);    //stores the desired size
         cv.resize(warpDst, warpDst, finalSize, 0, 0, cv.INTER_AREA);    //run the resize for the final desired dimension
+        if (stepElements.resize) {  //output resized image to element
+            cv.imshow(stepElements.resize, warpDst);
+        }
     }
 
     //cleanup matrices
